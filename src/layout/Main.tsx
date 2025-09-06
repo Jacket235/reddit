9bdd98b9e0304ea8fd0e36d1bb3d7f59ba7b6d80
Sebastian Mastalerz <97692547+Jacket235@users.noreply.github.com>
2025-09-06 21:27:38 +0200

    Main post container finished
    Along that, some bug fixes

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -16,13 +16,14 @@ export default function Main() {
                 </button>
             </div>
             <hr style={{margin: "8px 0 0 0"}}></hr>
-            <div className="content">
+            <div className="posts">
                 <article>
+                    <a href="article"></a>
                     <div className="credit-bar">
                         <span>
-                            <a>
-                                <span style={{paddingRight: "6px"}}>{icons.default_article_subreddit_icon}</span>
-                                r/subreddit
+                            <a href="cats">
+                                <span className="subreddit-icon">{icons.default_article_subreddit_icon}</span>
+                                r/cats
                             </a>
                             <span>•</span>
                             <TimeAgo date="2025-09-06T19:39:00" />
@@ -31,26 +32,69 @@ export default function Main() {
                             {icons.article_actions}
                         </button>
                     </div>
-                    <a>
-                        A very cute kitty I found on Google images.
-                    </a>
+                    <div className="post-title">
+                        <a>
+                            A very cute kitty I found on Google images.
+                        </a>
+                    </div>
                     <div style={{backgroundImage: `url(car.jpg)`}} className="post-media-container">
                         <img src="car.jpg"/>
                     </div>
                     <div className="action-row">
                         <span>
-                            <button>{icons.upvote}</button>
-                            12
-                            <button>{icons.downvote}</button>
+                            <button className="vote">{icons.upvote}</button>
+                            67
+                            <button className="vote">{icons.downvote}</button>
                         </span>
                         <span>
-                            <a>{icons.comment} 127</a>
+                            <a href="comments">{icons.comment} 12</a>
                         </span>
                         <span>
                             <button>{icons.reward}</button>
                         </span>
                         <span>
-                            <button>{icons.share} Share</button>
+                            <button>{icons.share}Share</button>
+                        </span>
+                    </div>
+                </article>
+                <hr></hr>
+                <article>
+                    <a href="article"></a>
+                    <div className="credit-bar">
+                        <span>
+                            <a href="bigdogs">
+                                <span className="subreddit-icon">{icons.default_article_subreddit_icon}</span>
+                                r/bigdogs
+                            </a>
+                            <span>•</span>
+                            <TimeAgo date="2025-09-06T21:25:00" />
+                        </span>
+                        <button>
+                            {icons.article_actions}
+                        </button>
+                    </div>
+                    <div className="post-title">
+                        <a>
+                            Some bear photo
+                        </a>
+                    </div>
+                    <div style={{backgroundImage: `url(bear.jpg)`}} className="post-media-container">
+                        <img src="bear.jpg"/>
+                    </div>
+                    <div className="action-row">
+                        <span>
+                            <button className="vote">{icons.upvote}</button>
+                            12
+                            <button className="vote">{icons.downvote}</button>
+                        </span>
+                        <span>
+                            <a href="comments">{icons.comment} 1</a>
+                        </span>
+                        <span>
+                            <button>{icons.reward}</button>
+                        </span>
+                        <span>
+                            <button>{icons.share}Share</button>
                         </span>
                     </div>
                 </article>
