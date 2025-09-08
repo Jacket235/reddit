e106b908095065eee5166a2f2c84de6e53c265f2
Sebastian Mastalerz <97692547+Jacket235@users.noreply.github.com>
2025-09-08 18:31:34 +0200

    Main sort section added

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,9 +1,20 @@
 import './main.css'
+import icons from '../components/Icons';
 
 export default function Main() {
     return (
         <main>
-            hey<br />    
+             <div className="top">
+                <button>
+                    <span>Best</span>
+                    <span>{icons.arrow}</span>
+                </button>
+                <button>
+                    <span>{icons.card_layout}</span>
+                    <span>{icons.arrow}</span>
+                </button>
+            </div>
+            <hr></hr>
         </main>
     );
 }
