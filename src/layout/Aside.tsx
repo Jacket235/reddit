7ad319352a00249481a7f797f6027ff872202c48
Sebastian Mastalerz <97692547+Jacket235@users.noreply.github.com>
2025-09-13 04:48:02 +0200

    Aside container finished

--- src/layout/Aside.tsx
+++ src/layout/Aside.tsx
@@ -15,16 +15,178 @@ export default function Aside() {
                         <div className="recent-post-subreddit-info">
                             <a>
                                 <span>{icons.default_subreddit_icon_aside} </span>
-                                <span>r/subreddit</span>
+                                <span>r/youtube</span>
                             </a>
                             <span>•</span>
                             <TimeAgo date="2025-09-11T01:56:00" />
                         </div>
                         <div className="recent-post-title">
-
+                            <a>
+                                <span>Video not uploading in HD</span>
+                            </a>
                         </div>
                     </div>
                     <div className="recent-post-action-row">
+                        <span>3 upvotes </span>
+                        <span>·</span>
+                        <span> 9 comments</span>
+                    </div>
+                </div>
+                <hr></hr>
+                <div className="recent-post">
+                    <div className="recent-post-info">
+                        <div className="recent-post-subreddit-info">
+                            <a>
+                                <span>{icons.default_subreddit_icon_aside} </span>
+                                <span>r/premiere</span>
+                            </a>
+                            <span>•</span>
+                            <TimeAgo date="2025-09-11T01:56:00" />
+                        </div>
+                        <div className="recent-post-title">
+                            <a>
+                                <span>How to Maintain Original Quality When Exporting in Premiere Pro?</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="recent-post-action-row">
+                        <span>1 upvote </span>
+                        <span>·</span>
+                        <span> 12 comments</span>
+                    </div>
+                </div>
+                <hr></hr>
+                <div className="recent-post">
+                    <div className="recent-post-info">
+                        <div className="recent-post-subreddit-info">
+                            <a>
+                                <span>{icons.default_subreddit_icon_aside} </span>
+                                <span>r/github</span>
+                            </a>
+                            <span>•</span>
+                            <TimeAgo date="2025-09-11T01:56:00" />
+                        </div>
+                        <div className="recent-post-title">
+                            <a>
+                                <span>How to get started with Github Desktop?</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="recent-post-action-row">
+                        <span>1 comment</span>
+                    </div>
+                </div>
+                <hr></hr>
+                <div className="recent-post">
+                    <div className="recent-post-info">
+                        <div className="recent-post-subreddit-info">
+                            <a>
+                                <span>{icons.default_subreddit_icon_aside} </span>
+                                <span>r/tailwindcss</span>
+                            </a>
+                            <span>•</span>
+                            <TimeAgo date="2025-09-11T01:56:00" />
+                        </div>
+                        <div className="recent-post-title">
+                            <a>
+                                <span>Some tailwind classes just don't seem to work?</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="recent-post-action-row">
+                        <span>8 upvotes </span>
+                        <span>·</span>
+                        <span> 15 comments</span>
+                    </div>
+                </div>
+                <hr></hr>
+                <div className="recent-post">
+                    <div className="recent-post-info">
+                        <div className="recent-post-subreddit-info">
+                            <a>
+                                <span>{icons.default_subreddit_icon_aside} </span>
+                                <span>r/youtube</span>
+                            </a>
+                            <span>•</span>
+                            <TimeAgo date="2025-09-11T01:56:00" />
+                        </div>
+                        <div className="recent-post-title">
+                            <a>
+                                <span>Video not uploading in HD</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="recent-post-action-row">
+                        <span>3 upvotes </span>
+                        <span>·</span>
+                        <span> 9 comments</span>
+                    </div>
+                </div>
+                <hr></hr>
+                <div className="recent-post">
+                    <div className="recent-post-info">
+                        <div className="recent-post-subreddit-info">
+                            <a>
+                                <span>{icons.default_subreddit_icon_aside} </span>
+                                <span>r/premiere</span>
+                            </a>
+                            <span>•</span>
+                            <TimeAgo date="2025-09-11T01:56:00" />
+                        </div>
+                        <div className="recent-post-title">
+                            <a>
+                                <span>How to Maintain Original Quality When Exporting in Premiere Pro?</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="recent-post-action-row">
+                        <span>1 upvote </span>
+                        <span>·</span>
+                        <span> 12 comments</span>
+                    </div>
+                </div>
+                <hr></hr>
+                <div className="recent-post">
+                    <div className="recent-post-info">
+                        <div className="recent-post-subreddit-info">
+                            <a>
+                                <span>{icons.default_subreddit_icon_aside} </span>
+                                <span>r/github</span>
+                            </a>
+                            <span>•</span>
+                            <TimeAgo date="2025-09-11T01:56:00" />
+                        </div>
+                        <div className="recent-post-title">
+                            <a>
+                                <span>How to get started with Github Desktop?</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="recent-post-action-row">
+                        <span>1 comment</span>
+                    </div>
+                </div>
+                <hr></hr>
+                <div className="recent-post">
+                    <div className="recent-post-info">
+                        <div className="recent-post-subreddit-info">
+                            <a>
+                                <span>{icons.default_subreddit_icon_aside} </span>
+                                <span>r/youtube</span>
+                            </a>
+                            <span>•</span>
+                            <TimeAgo date="2025-09-11T01:56:00" />
+                        </div>
+                        <div className="recent-post-title">
+                            <a>
+                                <span>Video not uploading in HD</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="recent-post-action-row">
+                        <span>3 upvotes </span>
+                        <span>·</span>
+                        <span> 9 comments</span>
                     </div>
                 </div>
             </div>
